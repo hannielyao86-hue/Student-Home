49cbf76ebbf28cb34c3f784a4cafd3262d26db4c
hanniel <hannielyao86@gmail.com>
2026-05-11 02:32:19 +0000

    commentaire

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
     /**
      * Méthode principale - point d'entrée de l'application
-     * @param args Arguments de ligne de commande
      */
     public static void main(String[] args) {
         System.out.println("La fonctionnalité room a été supprimée du projet.");
